8186f8c58fba1031c5f0702c47967367c0b53317
alessonJuley <alessonabao@gmail.com>
2023-10-27 19:48:42 +1300

    cleaned durationModel code

--- SentenceApp/CoreML/MultipleOffences/Duration.swift
+++ SentenceApp/CoreML/MultipleOffences/Duration.swift
@@ -23,39 +23,3 @@ func durationModel(offence: String, severity: Int, intent: String, remandDuratio
     
     return nil
 }
-
-func durationModel2(offence: String, severity: Int, intent: String, remandDuration: Int, sentence: String, sentenceType: String) -> DurationRegressorModelOutput?{
-    do{
-        // instantiate a new object model
-        let config = MLModelConfiguration()
-        // add model
-        let durationModel = try DurationRegressorModel(configuration: config)
-        // use model to recommend sentence
-        let recommendDuration = try durationModel.prediction(offence: offence, offenceCategory: Double(severity), intent: intent, remandDuration: Double(remandDuration), sentence: sentence, sentenceType: sentenceType)
-        return recommendDuration
-    }
-    catch{
-        print("[CoreML>MultipleOffences>Duration.swift]Something went wrong with duration model.")
-    }
-    
-    return nil
-}
-
-
-//func durationModel(severity: Int, remandDuration: Int, sentence: String, sentenceType: String) -> DurationTabularRegressorOutput?{
-//    do{
-//        // instantiate a new object model
-//        let config = MLModelConfiguration()
-//        // add model
-//        let durationModel = try DurationTabularRegressor(configuration: config)
-//        // use model to recommend sentence
-//        let recommendDuration = try durationModel.prediction(offenceCategory: Double(severity), remandDuration: Double(remandDuration), sentence: sentence, sentenceType: sentenceType)
-//
-//        return recommendDuration
-//    }
-//    catch{
-//        print("[CoreML>MultipleOffences>Duration.swift]Something went wrong with duration model.")
-//    }
-//
-//    return nil
-//}
