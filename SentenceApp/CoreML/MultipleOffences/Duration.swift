94121ed10cdbd14080afcb2748aacbd7574345b9
alessonJuley <alessonabao@gmail.com>
2023-10-27 19:50:53 +1300

    cleaned fineModel code and fixed other models code

--- SentenceApp/CoreML/MultipleOffences/Duration.swift
+++ SentenceApp/CoreML/MultipleOffences/Duration.swift
@@ -23,3 +23,22 @@ func durationModel(offence: String, severity: Int, intent: String, remandDuratio
     
     return nil
 }
+
+func durationModel2(offence: String, severity: Int, intent: String, remandDuration: Int, sentence: String, sentenceType: String) -> DurationRegressorModelOutput?{
+    do{
+        // instantiate a new object model
+        let config = MLModelConfiguration()
+        // add model
+        let durationModel = try DurationRegressorModel(configuration: config)
+        // use model to recommend sentence
+        let recommendDuration = try durationModel.prediction(offence: offence, offenceCategory: Double(severity), intent: intent, remandDuration: Double(remandDuration), sentence: sentence, sentenceType: sentenceType)
+        return recommendDuration
+    }
+    catch{
+        print("[CoreML>MultipleOffences>Duration.swift]Something went wrong with duration model.")
+    }
+    
+    return nil
+}
+
+
--- SentenceApp/CoreML/MultipleOffences/Fine.swift
+++ SentenceApp/CoreML/MultipleOffences/Fine.swift
@@ -43,20 +43,4 @@ func fineModel2(offence: String, severity: Int, intent: String, sentence: String
     return nil
 }
 
-//func fineModel(offence: String, severity: Int, intent: String, sentence: String, sentenceType: String) -> FinesTabularRegressorOutput?{
-//    do{
-//        // instantiate a new object model
-//        let config = MLModelConfiguration()
-//        // add model
-//        let fineModel = try FinesTabularRegressor(configuration: config)
-//        // use model to recommend sentence
-//        let recommendFine = try fineModel.prediction(offence: offence, offenceCategory: Double(severity), intent: intent, sentence: sentence, sentenceType: sentenceType)
-//
-//        return recommendFine
-//    }
-//    catch{
-//        print("[CoreML>MultipleOffences>Fine.swift]Something went wrong with fine model.")
-//    }
-//
-//    return nil
-//}
+
